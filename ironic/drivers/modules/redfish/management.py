97b7038cb4e721a90f160f90438070c4a82d3e69
Adam Rozman <adam.rozman@est.tech>
2025-03-07 11:32:54 +0200

    decrease the BMC calls during power sync
    This commit:
     - Modifies the way the BMC "system" information is handled during the
       power sync process
     - Adds a new function to redfish driver's management module to make
       the "system" data accessible via the management module
     - Makes redifsh management boot caching able to reuse existing system data
     - Makes redfish management vendor caching able to reuse existing system data
     - Makes ipmi management module vendor caching compatible with management
       interface
     - Makes ipmi management module provide an inert get_system function
     - Implements wrapper functions for get_boot_mode and get_secure_boot_status
       in the ipmi management module in order to be compatible with the
       modified interface behavior
     - Makes ipmi power module get_power_state compatible with management
       interface
    
    Change-Id: If2bf6b53bf4c197845651227368069e1ec66d25f
    Signed-off-by: Adam Rozman <adam.rozman@est.tech>

--- ironic/drivers/modules/redfish/management.py
+++ ironic/drivers/modules/redfish/management.py
@@ -511,7 +511,7 @@ class RedfishManagement(base.ManagementInterface):
 
         manager_utils.node_power_action(task, old_power_state)
 
-    def get_boot_mode(self, task):
+    def get_boot_mode(self, task, system=None):
         """Get the current boot mode for a node.
 
         Provides the current boot mode of the node.
@@ -523,9 +523,11 @@ class RedfishManagement(base.ManagementInterface):
         :returns: The boot mode, one of :mod:`ironic.common.boot_mode` or
                   None if it is unknown.
         """
-        system = redfish_utils.get_system(task.node)
+        current_system = system
+        if current_system is None:
+            current_system = redfish_utils.get_system(task.node)
 
-        return BOOT_MODE_MAP.get(system.boot.get('mode'))
+        return BOOT_MODE_MAP.get(current_system.boot.get('mode'))
 
     @staticmethod
     def _sensor2dict(resource, *fields):
@@ -630,6 +632,18 @@ class RedfishManagement(base.ManagementInterface):
                 node=getattr(task.node, 'uuid', 'unknown'),
                 reason="BMC clock verify step failed: %s" % str(e)
             )
+
+    def get_system(self, task):
+        """Get the redfish system endpoint for a node.
+
+        Provides the current redfisy system endpoint data of the node.
+
+        :param task: A task from TaskManager
+        :raises: RedfishConnectionError when it fails to connect to Redfish
+        :raises: RedfishError if the System is not registered in Redfish
+        :returns: The system endpoint, or None if it is unknown.
+        """
+        return redfish_utils.get_system(task.node)
 
     @classmethod
     def _get_sensors_fan(cls, chassis):
@@ -999,7 +1013,7 @@ class RedfishManagement(base.ManagementInterface):
                                 'component': component,
                                 'uuid': task.node.uuid})
 
-    def detect_vendor(self, task):
+    def detect_vendor(self, task, system=None):
         """Detects and returns the hardware vendor.
 
         Uses the System's Manufacturer field.
@@ -1012,7 +1026,12 @@ class RedfishManagement(base.ManagementInterface):
         :returns: String representing the BMC reported Vendor or
                   Manufacturer, otherwise returns None.
         """
-        return redfish_utils.get_system(task.node).manufacturer
+        manufacturer = None
+        if system is None:
+            manufacturer = redfish_utils.get_system(task.node).manufacturer
+        else:
+            manufacturer = system.manufacturer
+        return manufacturer
 
     @METRICS.timer('RedfishManagement.update_firmware')
     @base.clean_step(priority=0, abortable=False,
@@ -1342,7 +1361,7 @@ class RedfishManagement(base.ManagementInterface):
             firmware_utils.cleanup(node)
             raise error
 
-    def get_secure_boot_state(self, task):
+    def get_secure_boot_state(self, task, system=None):
         """Get the current secure boot state for the node.
 
         :param task: A task from TaskManager.
